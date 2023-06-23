c210dddd397877c90700acabb98fc66c01e79209
Daniyal <daniyalnaderi277@gmail.com>
2023-06-23 14:32:21 +0330

    add some

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import qrcode
 
-word = input('Enter a paragraph: ')
 
-qr = qrcode.QRCode(
-    version=3,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=15,
-    border=3,
-)
+with open('q.txt', 'r') as f:
+    for i in f.readlines():
+        word = str(i).strip('\n')
 
-qr.add_data(f'{word}')
-qr.make(fit=True)
+        qr = qrcode.QRCode(
+            version=3,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=2,
+            border=3,
+        )
 
-img = qr.make_image(fill_color="black", back_color="white")
-img.save(f"{word}.png")
+        qr.add_data(f'https://rashanovin.com/profile/{word}')
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save(f"{word}.png")
